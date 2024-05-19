9e1dd2e76f000a95cc7dfb344b0fea201c1dee0a
Mpumzi <mphumzimbula@gmail.com>
2024-05-19 18:22:52 +0200

    Added My name on the files I was responsible for

--- src/main/java/za/ac/cput/factory/WishListFactory.java
+++ src/main/java/za/ac/cput/factory/WishListFactory.java
@@ -8,6 +8,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class WishListFactory {
+
+    /**
+     * Mpumzi Mbula
+     * 219053324
+     * 17/05/2024
+     *
+     */
     public static  WishList buildWishList(String wishListName, List<ComicBook> comicBookList, LocalDate createdDate, LocalDate updatedDate){
         if(Helper.isStringNullorEmpty(wishListName)||Helper.isListNullorEmpty(comicBookList)|| !Helper.isNotAfter(createdDate,LocalDate.now())||Helper.isValidDate(updatedDate)){
             return null;
--- src/main/java/za/ac/cput/repository/WishListRepository.java
+++ src/main/java/za/ac/cput/repository/WishListRepository.java
@@ -1,7 +1,7 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.domain.Cart;
+import za.ac.cput.domain.WishList;
 
 /**
  * Mpumzi Mbula
@@ -9,5 +9,5 @@ import za.ac.cput.domain.Cart;
  * 17/05/2024
  *
  */
-public interface CartRepository  extends JpaRepository<Cart,Long>{
+public interface WishListRepository extends JpaRepository<WishList,Long> {
 }
