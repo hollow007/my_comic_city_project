69798ea49141f36533dc9d0eab557a914dcc56b4
Joshua@mycput <104217528+Fluffy-J@users.noreply.github.com>
2024-05-23 04:22:53 +0200

    Update AddressRepository.java
    Added student details

--- src/main/java/za/ac/cput/repository/AddressRepository.java
+++ src/main/java/za/ac/cput/repository/AddressRepository.java
@@ -1,3 +1,8 @@
+
+//Joshua Mokwebo
+//Student No 222191562
+//GitHubRepository:My_commic_city_project
+
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
