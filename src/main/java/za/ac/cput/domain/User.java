2d506a378d217f19c392c2fb5610165416d4513e
Mpumzi <mphumzimbula@gmail.com>
2024-06-20 22:52:07 +0200

    Added fixed User

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -1,94 +1,33 @@
 package za.ac.cput.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 
-import java.util.Objects;
-@Entity
+import jakarta.persistence.Embedded;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+//
 
-public class User {
-    @Id
-    @GeneratedValue
-    private String userName;
-    private String firstName ;
-    private String lastName;
 
-    public User() {}
+@MappedSuperclass
+public class User{
+    protected String password;
+    @Embedded
+    protected Name name;
+    @OneToOne
+    protected Contact contact;
 
-    private User (UserBuilder userBulider){
-        this.userName = userBulider.userName;
-        this.firstName = userBulider.firstName;
-        this.lastName = userBulider.lastName;
+
+    public String getPassword() {
+        return password;
     }
 
-    public String getUserName() {
-        return userName;
+    public Name getName() {
+        return name;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public Contact getContact() {
+        return contact;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(userName, user.userName) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userName, firstName, lastName);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userName='" + userName + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
-
-    public static class UserBuilder{
-
-        protected String userName;
-        protected String firstName ;
-        protected String lastName;
-
-        public UserBuilder setUsername(String userName) {
-            this.userName = userName;
-            return this;
-        }
-
-        public UserBuilder setFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public UserBuilder setLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-
-        public UserBuilder copy (User o){
-            this.userName = o.userName;
-            this.firstName = o.firstName;
-            this.lastName = o.lastName;
-            return this;
-        }
-
-
-        public User build(){return new User(this);}
-    }
 
 
 }
