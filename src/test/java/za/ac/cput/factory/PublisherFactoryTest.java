b037233e5b754b9b6272232c113573f1272ef257
Mbasa Cabane <221307311@mycput.ac.za>
2024-06-23 19:26:22 +0200

    Helper, Publisher Factory and Test updated

--- src/test/java/za/ac/cput/factory/PublisherFactoryTest.java
+++ src/test/java/za/ac/cput/factory/PublisherFactoryTest.java
@@ -2,28 +2,37 @@ package za.ac.cput.factory;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Author;
+import za.ac.cput.domain.ComicBook;
 import za.ac.cput.domain.Publisher;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PublisherFactoryTest {
+
     private Publisher publisher1;
     private Publisher publisher2;
-
     @BeforeEach
     void setUp() {
-        publisher1 = PublisherFactory.buildPublisher(34655, "Cabane Publishers",2000);
-        publisher2 = publisher1;
+
+
+        publisher1 = PublisherFactory.buildPublisher(34655L, "Marvel",2000);
+
+        publisher2 = PublisherFactory.buildPublisher(34655L, "C Publishers", 2000);
     }
 
     @Test
-    void buildPublisher() {
+    void testBuildPublisher() {
         assertNotNull(publisher1);
         System.out.println(publisher1);
     }
     @Test
-    void identityTest(){
-        assertSame(publisher1, publisher2);
+    void failingTestBuildPublisher() {
+        assertNotNull(publisher2);
         System.out.println(publisher2);
     }
 }
