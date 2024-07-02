2b288892bbcc4bdb43d2852b4e370239879bb326
Mlungisi <mmbuyazi@gmail.com>
2024-07-02 21:15:31 +0200

    Factories

---
+++ src/main/java/za/ac/cput/Service/comicBookService/ComicBookService.java
@@ -0,0 +1,40 @@
+package za.ac.cput.service.comicBookService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.ComicBook;
+
+import za.ac.cput.repository.ComicBookRepository;
+
+import java.util.List;
+@Service
+public class ComicBookService implements IComicBookService{
+    @Autowired
+    private ComicBookRepository repo;
+
+    @Override
+    public ComicBook create(ComicBook comicBook) {
+        return repo.save(comicBook);
+    }
+
+    @Override
+    public ComicBook read(Long s) {
+        return repo.findById(s).orElse(null);
+    }
+
+    @Override
+    public ComicBook update(ComicBook comicBook) {
+        return repo.save(comicBook);
+    }
+
+    @Override
+    public boolean delete(Long s) {
+        repo.deleteById(s);
+        return !repo.existsById(s);
+    }
+
+    @Override
+    public List<ComicBook> getAll() {
+        return repo.findAll();
+    }
+}
