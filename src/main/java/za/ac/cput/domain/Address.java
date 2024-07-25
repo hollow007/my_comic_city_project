e38a1130d6abb16acd884922207f843992ea5ccc
Fararani Joshua <joshua@kineticskunk.com>
2024-07-25 10:11:40 +0200

    The address class has been modified

--- src/main/java/za/ac/cput/domain/Address.java
+++ src/main/java/za/ac/cput/domain/Address.java
@@ -9,38 +9,29 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 
-@Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "Address_type", discriminatorType = DiscriminatorType.STRING)
+// What have  i done ,  I have changed the adress class  and made it into  a Pojo
+// The Billing Address and shipping Address will then Inherit from this class excluding the ID 
+
+
 
 
 public class Address {
-
-    //@GeneratedValue(strategy = GenerationType.SEQUENCE) // made a change to the generationType please see >> https://www.baeldung.com/jpa-get-auto-generated-id
-    @Id
-    @Column(name="System_ID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
+    
     protected String street;
     protected String suburb;
     protected String city;
-
     protected String postalCode;
 
     public Address() {
     }
 
     Address(AddressBuilder builder) {
-        this.id=builder.id;
         this.street = builder.street;
         this.suburb = builder.suburb;
         this.city = builder.city;
         this.postalCode = builder.postalCode;
     }
 
-    public Long getId() {
-        return id;
-    }
 
     public String getStreet() {
         return street;
@@ -62,18 +53,17 @@ public class Address {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Address address)) return false;
-        return Objects.equals(id, address.id) && Objects.equals(street, address.street) && Objects.equals(suburb, address.suburb) && Objects.equals(city, address.city) && Objects.equals(postalCode, address.postalCode);
+        return Objects.equals(street, address.street) && Objects.equals(suburb, address.suburb) && Objects.equals(city, address.city) && Objects.equals(postalCode, address.postalCode);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, street, suburb, city, postalCode);
+        return Objects.hash(street, suburb, city, postalCode);
     }
 
     @Override
     public String toString() {
         return "Address{" +
-                "id=" + id +
                 ", street='" + street + '\'' +
                 ", suburb='" + suburb + '\'' +
                 ", city='" + city + '\'' +
@@ -82,17 +72,12 @@ public class Address {
     }
 
     public static class AddressBuilder {
-        protected Long id;
         protected String street;
         protected String suburb;
         protected String city;
         protected String postalCode;
 
-        public AddressBuilder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
+    
         public AddressBuilder setStreet(String street) {
             this.street = street;
             return this;
@@ -114,7 +99,6 @@ public class Address {
         }
 
         public AddressBuilder copy(Address address) {
-            this.id= address.id;
             this.street = address.street;
             this.suburb = address.suburb;
             this.city = address.suburb;
