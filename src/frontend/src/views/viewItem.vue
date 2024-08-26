2ef278550111afb2dae95e9c6f0146046b5c9b0b
Joshua@mycput <222191562@mycput.ac.za>
2024-08-26 16:28:42 +0200

    The view item look and feel is done

---
+++ src/frontend/src/views/viewItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="view_item">
+    <ProductComponent/>
+  </div>
+</template>
+
+<script>
+import ProductComponent from "@/components/productComponent.vue";
+export default {
+  name: 'viewItem',
+  components: {
+    ProductComponent
+  }
+}
+</script>
